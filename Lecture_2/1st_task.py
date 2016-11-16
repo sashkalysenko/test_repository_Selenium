6557541f7a592ea7c601db42b06169c18f7135e8
Aleksandr <sashka.lysenko@gmail.com>
2016-11-17 01:41:23 +0200

    1st_task
    1st task to create simple test and share it via repository
    (2nd lecture)

---
+++ Lecture_2/1st_task.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_simple_one(driver):
+    driver.get("http://www.bigmir.net")
+    WebDriverWait(driver, 10)
