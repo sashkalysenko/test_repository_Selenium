814130d739fe5aacbfe30b75a5be214f1100a740
Aleksandr <sashka.lysenko@gmail.com>
2016-11-25 01:25:17 +0200

    7th task - beggining

---
+++ Lecture_4/7th_task.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def admin_login(driver):
+    driver.get("http://localhost/litecart/admin")
+    assert "My Store" in driver.title
+    driver.find_element_by_css_selector("div#box-login [name = username]").send_keys("admin")
+    driver.find_element_by_css_selector("div#box-login [name = password]").send_keys("admin")
+    driver.find_element_by_css_selector("button[name = login]").click()
+
+
+def test_go_through_vertical_menu(driver):
+    admin_login(driver)
+    sidebar = driver.find_element_by_css_selector("td#sidebar")
+    main_menues = sidebar.find_elements_by_css_selector("li#app-")
