5d85666456fe0e99be3aca2e09c36e0bc54acd22
Aleksandr <sashka.lysenko@gmail.com>
2016-12-01 13:58:03 +0200

    9th_task_fully_completed

--- Lecture_5/9th_task.py
+++ Lecture_5/9th_task.py
@@ -19,9 +19,9 @@ def admin_login(driver):
     driver.find_element_by_css_selector("button[name = login]").click()
 
 
-def id_for_column(driver, column_name):
+def id_for_column(driver, table, column_name):
     """return id of column name or False"""
-    header = driver.find_elements_by_css_selector("form[name = countries_form] tr.header th")
+    header = driver.find_elements_by_css_selector(table + " tr.header th")
     header_id = 0
     for element in header:
         if element.text == column_name:
@@ -37,28 +37,51 @@ def test_alphabetical_order_counties(driver):
     driver.find_element_by_css_selector("a[href *= countries]").click()
     rows = driver.find_elements_by_css_selector("form[name = countries_form] tr.row")
     # get ides of needed columns
-    column_id = id_for_column(driver, "ID")
-    column_name_id = id_for_column(driver, "Name")
-    column_zones_id = id_for_column(driver, "Zones")
+    countries_table = "form[name = countries_form]"
+    column_code_id = id_for_column(driver, countries_table, "Code")
+    column_name_id = id_for_column(driver, countries_table, "Name")
+    column_zones_id = id_for_column(driver, countries_table, "Zones")
     # list for all countries
     countries_list = []
     # list for countries with time zones > 1
-    id_countries_with_zones = []
+    country_code_with_zones = []
 
-    # loop to fill lists which noted above
+    # loop to fill lists which are noted above
     for row in rows:
         cells = row.find_elements_by_css_selector("td")
-        row_id = int(cells[column_id].text)
-        countries_list.append(cells[column_name_id].text)
-        amount_of_zones = int(cells[column_zones_id].text)
+        country_code = cells[column_code_id].get_attribute('textContent')
+        countries_list.append(cells[column_name_id].get_attribute('textContent'))
+        amount_of_zones = int(cells[column_zones_id].get_attribute('textContent'))
         if amount_of_zones > 1:
-            id_countries_with_zones.append(row_id)
+            country_code_with_zones.append(country_code)
 
     # verify alphabetical order of all countries
     correct_order = countries_list[:]
     correct_order.sort()
     assert countries_list == correct_order
 
+    # verify alphabetical order of time zones for each country which has them
+    for country in country_code_with_zones:
+        # navigate to country with timezones
+        driver.find_element_by_css_selector("a[href*='country_code={0}']".format(country)).click()
+        # define table with zones
+        zones_table = "table#table-zones"
+        # get rows from zones_table
+        rows = driver.find_elements_by_css_selector(zones_table + " tr:not([class = header])")
+        # get id of column Name
+        zones_name_id = id_for_column(driver, zones_table, "Name")
+        # list for zones
+        zones_list = []
+        # loop to fill zones_list from current country
+        for row in rows:
+            cells = row.find_elements_by_css_selector("td")
+            zone_name = cells[zones_name_id].get_attribute('textContent')
+            if zone_name:
+                zones_list.append(zone_name)
 
-
-
+        # verify alphabetical order of all zones
+        correct_order = zones_list[:]
+        correct_order.sort()
+        assert zones_list == correct_order
+        # come back to Countries menu
+        driver.find_element_by_css_selector("span.button-set button[name = cancel]").click()
