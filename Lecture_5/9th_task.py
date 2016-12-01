1fe5415b3b34c092309626417088c1f83b5e550e
Aleksandr <sashka.lysenko@gmail.com>
2016-12-01 02:01:10 +0200

    9th_task

--- Lecture_5/9th_task.py
+++ Lecture_5/9th_task.py
@@ -19,5 +19,46 @@ def admin_login(driver):
     driver.find_element_by_css_selector("button[name = login]").click()
 
 
+def id_for_column(driver, column_name):
+    """return id of column name or False"""
+    header = driver.find_elements_by_css_selector("form[name = countries_form] tr.header th")
+    header_id = 0
+    for element in header:
+        if element.text == column_name:
+            return header_id
+        else:
+            header_id += 1
+    return False
+
+
 def test_alphabetical_order_counties(driver):
-    driver.find_element_by_css_selector("")
+    # login and navigate to Countries-page
+    admin_login(driver)
+    driver.find_element_by_css_selector("a[href *= countries]").click()
+    rows = driver.find_elements_by_css_selector("form[name = countries_form] tr.row")
+    # get ides of needed columns
+    column_id = id_for_column(driver, "ID")
+    column_name_id = id_for_column(driver, "Name")
+    column_zones_id = id_for_column(driver, "Zones")
+    # list for all countries
+    countries_list = []
+    # list for countries with time zones > 1
+    id_countries_with_zones = []
+
+    # loop to fill lists which noted above
+    for row in rows:
+        cells = row.find_elements_by_css_selector("td")
+        row_id = int(cells[column_id].text)
+        countries_list.append(cells[column_name_id].text)
+        amount_of_zones = int(cells[column_zones_id].text)
+        if amount_of_zones > 1:
+            id_countries_with_zones.append(row_id)
+
+    # verify alphabetical order of all countries
+    correct_order = countries_list[:]
+    correct_order.sort()
+    assert countries_list == correct_order
+
+
+
+
